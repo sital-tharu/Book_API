94f780052cffa6d3c2c3786ebd236e731a49b912
sital-tharu <sitatharu769@gmail.com>
2025-09-07 00:20:01 +0530

    Accept data from post and pass it to service

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book } from './book.schema';
 
 @Controller('book')
-export class BookController {}
+export class BookController {
+    constructor(private readonly bookService: BookService){}
+
+    @Post()
+    async createBook(@Body () data: Book){
+    return this.bookService.create(data);
+    }
+}
