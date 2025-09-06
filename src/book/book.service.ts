5f1624ba2ae13dfddc2c2f1fd322ee034ed7f75e
sital-tharu <sitatharu769@gmail.com>
2025-09-07 00:07:05 +0530

    add new document to Mongo

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Book, BookDocument } from './book.schema';
 import { Model } from 'mongoose';
+import { async } from 'rxjs';
 
 @Injectable()
 export class BookService {
     constructor(
     @InjectModel(Book.name) private bookModel : Model<BookDocument>
     ){}
+    async create(book: Book): Promise<Book> {
+    return new this.bookModel(book).save();
+}
 }
