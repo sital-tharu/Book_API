95a4744684a574758bd972a2e52070aad759ed47
sital-tharu <sitatharu769@gmail.com>
2025-09-06 22:45:23 +0530

    Generate book module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BookModule } from './book/book.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URI!),
+    BookModule,
   ],
   controllers: [AppController],
   providers: [AppService],
