8ef256730f1bb9dc8dd1f63350312b458658db25
sital-tharu <sitatharu769@gmail.com>
2025-09-07 00:54:34 +0530

    fix: register BookController and BookService in BookModule to enable /book endpoint

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Mongoose } from 'mongoose';
 import { Book, BookSchema } from './book.schema';
+import { BookController } from './book.controller';
+import { BookService } from './book.service';
 
 @Module({
-    imports :[ 
-        MongooseModule.forFeature([{name : Book.name, schema : BookSchema}])
-    ]
+  imports: [
+    MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }])
+  ],
+  controllers: [BookController],
+  providers: [BookService],
 })
 export class BookModule {}
