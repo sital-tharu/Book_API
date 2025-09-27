d57de50ce7dd3937a71c038a699da4550938a2b1
sital-tharu <sitatharu769@gmail.com>
2025-09-27 17:59:13 +0530

    CLI: generate resolver book/resolvers/book

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -3,12 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Book, BookSchema } from './book.schema';
 import { BookController } from './book.controller';
 import { BookService } from './book.service';
+import { BookResolver } from './resolvers/book/book.resolver';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }])
   ],
   controllers: [BookController],
-  providers: [BookService],
+  providers: [BookService, BookResolver],
 })
 export class BookModule {}
