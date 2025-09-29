d8d38ef6f808a8869159a5c260695d41bbdc22f9
sital-tharu <sitatharu769@gmail.com>
2025-09-29 17:32:20 +0530

    read _id

--- src/book/model/book.model.ts
+++ src/book/model/book.model.ts
@@ -1,3 +1,10 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 import { ObjectType, Field, ID } from "@nestjs/graphql";
+
+@Schema()
+@ObjectType()
+export class Book extends Document {
+    @Field(() => ID)
+    declare readonly _id: string;
+}
