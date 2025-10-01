13530d4816338f91b1ac104ae022c6889b732b47
sital-tharu <sitatharu769@gmail.com>
2025-10-01 16:17:56 +0530

    feat(book): extend CreateBookInput DTO with author and description fields

--- src/book/dto/create-book.input.ts
+++ src/book/dto/create-book.input.ts
@@ -1,10 +1,19 @@
-import { InputType , Field  } from "@nestjs/graphql";
-import { IsNotEmpty, IsString  } from "class-validator";
+import { InputType, Field } from "@nestjs/graphql";
+import { IsNotEmpty, IsString } from "class-validator";
 
-@InputType() 
+@InputType()
 export class createBookInput {
     @Field()
     @IsString()
     @IsNotEmpty()
     title: string;
+
+    @Field({ nullable: true })
+    @IsString()
+    dectription?: string;
+
+    @Field()
+    @IsString()
+    @IsNotEmpty()
+    author: string;
 }
