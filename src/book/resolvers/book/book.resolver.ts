f405752b42949da18a7d0a3ada7a23e6d401df45
sital-tharu <sitatharu769@gmail.com>
2025-10-01 18:22:34 +0530

    feat(book): add create, update, and delete mutations to BookResolver

--- src/book/resolvers/book/book.resolver.ts
+++ src/book/resolvers/book/book.resolver.ts
@@ -1,6 +1,36 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Book } from 'src/book/book.schema';
+import { BookService } from 'src/book/book.service';
+import { createBookInput } from 'src/book/dto/create-book.input';
+import { updateBookInput } from 'src/book/dto/update-book.input';
 
 @Resolver()
 export class BookResolver {
-    constructor 
+    constructor(private readonly bookService: BookService) { }
+
+    @Query(() => [Book], { name: 'getAllBooks' })
+    async findAll() {
+        return this.bookService.findAll();
+    }
+
+    @Query(() => Book, { name: 'getBook' })
+    async findOne(@Args('id', { type: () => String }) id: string) {
+        return this.bookService.findOne(id);
+    }
+
+    @Mutation(() => Book)
+    async createBook(@Args('input') input: createBookInput) {
+        return this.bookService.create(input);
+    }
+
+    @Mutation(() => Book)
+    async updateteBook(@Args('input') input: updateBookInput) {
+        return this.bookService.update(input);
+    }
+    @Mutation(() => Book)
+    async deleteeBook(@Args('id', { type: () => String }) id: string) {
+        return this.bookService.remove(id);
+    }
+
+
 }
