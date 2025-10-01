acda94eca0752b4d9a50434070a85f05389c5fa3
sital-tharu <sitatharu769@gmail.com>
2025-10-01 17:44:48 +0530

    feat(book): add update and remove methods to BookService with error handling

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,46 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Book, BookDocument } from './book.schema';
 import { Model } from 'mongoose';
 import { createBookInput } from './dto/create-book.input';
+import { updateBookInput } from './dto/update-book.input';
 
 
 @Injectable()
 export class BookService {
-    constructor(
-    @InjectModel(Book.name) private bookModel : Model<BookDocument>
-    ){}
-    async create(input: createBookInput): Promise<Book> {
-        const created = new this.bookModel(input);
-        return created.save();
-}
-    async findAll(): Promise<Book[]> {
-        return this.bookModel.find().exec();
-    }
-
-    async findOne(id:string): Promise <Book |null> {
-        return this.bookModel.findById(id).exec();
-    }
-
-    async updateStudent(id: string, book: Partial<Book>): Promise<Book | null>{
-      const updated = await this.bookModel.findByIdAndUpdate(id, {
-        title: book.title ?? null,
-        author: book.author ?? null,
-        year: book.year ?? null
-      },{ overwrite: true, new: true});
-      return updated;
-      }
-    
-    
-    
-
-
-   async update(id: string, book: Partial<Book>): Promise<Book | null> {
-    return this.bookModel.findByIdAndUpdate(id, book, { new: true }).exec();
-  } 
-
-  async delete(id: string): Promise<Book | null> {
-    return this.bookModel.findByIdAndDelete(id).exec();
+  constructor(
+    @InjectModel(Book.name) private bookModel: Model<BookDocument>
+  ) { }
+  async create(input: createBookInput): Promise<Book> {
+    const created = new this.bookModel(input);
+    return created.save();
+  }
+  async findAll(): Promise<Book[]> {
+    return this.bookModel.find().exec();
   }
 
+  async findOne(id: string): Promise<Book | null> {
+    return this.bookModel.findById(id).exec();
+  }
+
+  async update(input: updateBookInput):
+    Promise<Book> {
+    const existingBook = await this.bookModel.findById(input.id);
+    if (!existingBook) throw new NotFoundException('Book not Found!')
+    Object.assign(existingBook, input);
+    return existingBook.save();
+  }
+  async remove(id: string): Promise<boolean> {
+    const result = await this.bookModel.findByIdAndDelete(id);
+    if (!result) throw new NotFoundException('Book not Found!')
+    return true;
+  }
 }
