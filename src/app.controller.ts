a5061a45f8aa7d245cdea79a4f50c00dba2b4c01
sital-tharu <sitatharu769@gmail.com>
2025-10-06 22:00:19 +0530

    fet: implement guards in route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Throttle } from '@nestjs/throttler';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
+  @Throttle({ global: { limit: 5, ttl: 60000 } })
   getHello(): string {
-    return this.appService.getHello();
+    return 'This is route is throttled to 5 requests per minute.';
   }
 }
